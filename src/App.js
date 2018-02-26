20bd89921842f63a71e41e59fc289dfb330afed7
WeiKee <weikee070194@gmail.com>
2018-02-26 21:32:59 +0800

    handler practice

--- src/App.js
+++ src/App.js
@@ -1,37 +1,53 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   // 只有在extends Compenent 情况下才可以用state 其他都不行，
   // 必须小心使用state 因为会rerender
   // state 是在该component 里面， 而props 是从外面进来
   state = {
-    persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Menu', age: 27 }
-    ]
-  }
+    persons: [{ name: "Max", age: 28 }, { name: "Menu", age: 27 }],
+    showPersons: false
+  };
 
   switchNameHandler = () => {
     // console.log('Was clicked!');
     this.setState({
-      persons: [
-        { name: 'Max', age: 35 },
-        { name: 'Menu', age: 30 }
-      ]
-    })
+      persons: [{ name: "Max", age: 35 }, { name: "Menu", age: 30 }]
+    });
+  };
+
+  nameChangedHandler = event => {
+    this.setState({
+      persons: [{ name: "Max", age: 35 }, { name: event.target.value, age: 30 }]
+    });
+  };
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
   };
 
   render() {
     return (
       <div className="App">
         <h1>Hello World!</h1>
-        <button onClick={this.switchNameHandler}>SwitchName</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
-          click={this.switchNameHandler}
-        />
+        <button onClick={this.togglePersonHandler}>ToggleName</button>
+        {this.state.showPersons ? (
+          <div>
+            <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age}
+            />
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+              click={this.switchNameHandler}
+              changed={this.nameChangedHandler}
+            />
+          </div>
+        ) : null}
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React from "react";
 
+import "./Person.css";
 // functional component
-const person = (props) => {
-    return <p onClick={props.click}>I am a {props.name} with age {props.age}!</p>;
+const person = props => {
+  return (
+    <div className="Person">
+      <p onClick={props.click}>
+        I am a {props.name} with age {props.age}!
+      </p>
+      <input type="text" value={props.name} onChange={props.changed} />
+    </div>
+  );
 };
 
 export default person;
